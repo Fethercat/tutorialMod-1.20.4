ee5e896279a034fb8cf11301c6ab8d792a90c910
Andy <chenmingachang@gmail.com>
2024-09-02 13:17:54 -0700

    Added Item: "Ruby"

--- src/main/java/net/shadowedleaves/tutorialmod/TutorialMod.java
+++ src/main/java/net/shadowedleaves/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.shadowedleaves.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.shadowedleaves.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
 	}
 }
