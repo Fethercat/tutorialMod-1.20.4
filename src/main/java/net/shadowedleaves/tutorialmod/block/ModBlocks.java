e7737c24f54d8edbc0263475672d425964af3efc
ShadowedLeaves <chenmingachang@gmail.com>
2024-09-02 17:58:32 -0700

    Added Block: "Block of Raw Ruby"

--- src/main/java/net/shadowedleaves/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/shadowedleaves/tutorialmod/block/ModBlocks.java
@@ -15,6 +15,8 @@ import net.shadowedleaves.tutorialmod.TutorialMod;
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+    public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
 
     private static Block registerBlock(String name, Block block) {
