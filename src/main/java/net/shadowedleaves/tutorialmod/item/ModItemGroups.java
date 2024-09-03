25e402148756bf84f03415cb6c5fd00e7483c913
ShadowedLeaves <chenmingachang@gmail.com>
2024-09-02 22:05:56 -0700

    Added Custom Loot Tables

--- src/main/java/net/shadowedleaves/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/shadowedleaves/tutorialmod/item/ModItemGroups.java
@@ -19,6 +19,10 @@ public class ModItemGroups {
                         entries.add(ModItems.RAW_RUBY);
                         entries.add(ModBlocks.RUBY_BLOCK);
                         entries.add(ModBlocks.RAW_RUBY_BLOCK);
+                        entries.add(ModBlocks.RUBY_ORE);
+                        entries.add(ModBlocks.DEEPSLATE_RUBY_ORE);
+                        entries.add(ModBlocks.NETHER_RUBY_ORE);
+                        entries.add(ModBlocks.END_STONE_RUBY_ORE);
                     }).build());
 
     public static void registerItemGroups() {
