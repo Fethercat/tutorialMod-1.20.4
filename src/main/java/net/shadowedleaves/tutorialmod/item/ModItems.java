ff13935a627eef244cff198565ed3a8c00d970d3
ShadowedLeaves <chenmingachang@gmail.com>
2024-09-04 15:32:03 -0700

    Added Item: Metal Detector

--- src/main/java/net/shadowedleaves/tutorialmod/item/ModItems.java
+++ src/main/java/net/shadowedleaves/tutorialmod/item/ModItems.java
@@ -9,10 +9,13 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.shadowedleaves.tutorialmod.TutorialMod;
+import net.shadowedleaves.tutorialmod.item.custom.MetalDetectorItem;
 
 public class ModItems {
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+    public static final Item METAL_DETECTOR = registerItem("metal_detector",
+            new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
