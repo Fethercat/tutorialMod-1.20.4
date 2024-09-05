ba158a8532228fb8d4070f71586a4cf59f94af3c
ShadowedLeaves <chenmingachang@gmail.com>
2024-09-04 21:53:31 -0700

    Added Food Item: Tomato
    Tomato has a 50% chance of giving Health Boost for 1 minute.
    The Tomato is an "Always Edible" food.

---
+++ src/main/java/net/shadowedleaves/tutorialmod/item/ModFoodComponents.java
@@ -0,0 +1,12 @@
+package net.shadowedleaves.tutorialmod.item;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+    public static final FoodComponent TOMATO = new FoodComponent.Builder().hunger(3).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 1200), 0.5f)
+            .alwaysEdible()
+            .build();
+}
