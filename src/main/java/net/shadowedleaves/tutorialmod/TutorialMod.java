32a0124e1e1df1b72de5c792f4fe2aee651265f0
ShadowedLeaves <chenmingachang@gmail.com>
2024-09-04 23:24:42 -0700

    Added Fuel Item: Coal Briquette

--- src/main/java/net/shadowedleaves/tutorialmod/TutorialMod.java
+++ src/main/java/net/shadowedleaves/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.shadowedleaves.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.shadowedleaves.tutorialmod.block.ModBlocks;
 import net.shadowedleaves.tutorialmod.item.ModItemGroups;
 import net.shadowedleaves.tutorialmod.item.ModItems;
@@ -17,5 +18,6 @@ public class TutorialMod implements ModInitializer {
 		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+		FuelRegistry.INSTANCE.add(ModItems.COAL_BRIQUETTE, 200);
 	}
 }
