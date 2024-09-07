ab208187d6437bba53020429c226e5635e9a8d97
ShadowedLeaves <chenmingachang@gmail.com>
2024-09-06 22:41:19 -0700

    Added Armor: Ruby Armor
    Ruby Helmet
    Ruby Chestplate
    Ruby Leggings
    Ruby Boots

--- src/main/java/net/shadowedleaves/tutorialmod/datagen/ModItemTagProvider.java
+++ src/main/java/net/shadowedleaves/tutorialmod/datagen/ModItemTagProvider.java
@@ -3,6 +3,8 @@ package net.shadowedleaves.tutorialmod.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
+import net.shadowedleaves.tutorialmod.item.ModItems;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -13,6 +15,7 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup arg) {
-
+        getOrCreateTagBuilder(ItemTags.TRIMMABLE_ARMOR)
+                .add(ModItems.RUBY_HELMET, ModItems.RUBY_CHESTPLATE, ModItems.RUBY_LEGGINGS, ModItems.RUBY_BOOTS);
     }
 }
--- src/main/java/net/shadowedleaves/tutorialmod/item/ModItems.java
+++ src/main/java/net/shadowedleaves/tutorialmod/item/ModItems.java
@@ -29,6 +29,14 @@ public class ModItems {
             new SwordItem(ModToolMaterial.RUBY, 5, 3f, new FabricItemSettings()));
     public static final Item RUBY_HOE = registerItem("ruby_hoe",
             new HoeItem(ModToolMaterial.RUBY, 0, 0f, new FabricItemSettings()));
+    public static final Item RUBY_HELMET = registerItem("ruby_helmet",
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.HELMET, new FabricItemSettings()));
+    public static final Item RUBY_CHESTPLATE = registerItem("ruby_chestplate",
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()));
+    public static final Item RUBY_LEGGINGS = registerItem("ruby_leggings",
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.LEGGINGS, new FabricItemSettings()));
+    public static final Item RUBY_BOOTS = registerItem("ruby_boots",
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.BOOTS, new FabricItemSettings()));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
