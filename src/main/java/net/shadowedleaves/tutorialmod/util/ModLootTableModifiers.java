3bdd4df39da177f763f39025ac4fad84926577e3
ShadowedLeaves <chenmingachang@gmail.com>
2024-09-07 23:12:13 -0700

    Modified: ModLootTableModifiers.java

--- src/main/java/net/shadowedleaves/tutorialmod/util/ModLootTableModifiers.java
+++ src/main/java/net/shadowedleaves/tutorialmod/util/ModLootTableModifiers.java
@@ -1,8 +1,7 @@
 package net.shadowedleaves.tutorialmod.util;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.minecraft.loot.LootTable;
-import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.loot.LootTables;
 import net.shadowedleaves.tutorialmod.item.ModItems;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
@@ -11,10 +10,6 @@ import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class ModLootTableModifiers {
     private static final Identifier JUNGLE_TEMPLE_ID =
@@ -26,7 +21,7 @@ public class ModLootTableModifiers {
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            if(JUNGLE_TEMPLE_ID.equals(id)) {
+            if (JUNGLE_TEMPLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(0.5f))
@@ -36,7 +31,7 @@ public class ModLootTableModifiers {
                 tableBuilder.pool(poolBuilder.build());
             }
 
-            if(CREEPER_ID.equals(id)) {
+            if (CREEPER_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(0.25f))
@@ -45,17 +40,13 @@ public class ModLootTableModifiers {
 
                 tableBuilder.pool(poolBuilder.build());
             }
+
+            if (key == LootTables.DESERT_PYRAMID_ARCHAEOLOGY) {
+                tableBuilder.modifyPools(builder -> {
+                    builder.with(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
+                    builder.with(ItemEntry.builder(ModItems.COAL_BRIQUETTE).build());
+                });
+            }
         });
-//        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
-//            if(SUSPICIOUS_SAND_ID.equals(id)) {
-//                List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
-//                entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
-//                entries.add(ItemEntry.builder(ModItems.COAL_BRIQUETTE).build());
-//
-//                LootPool.Builder pool = LootPool.builder().with(entries);
-//                return LootTable.builder().pool(pool).build();
-//            }
-//            return null;
-//        });
     }
 }
