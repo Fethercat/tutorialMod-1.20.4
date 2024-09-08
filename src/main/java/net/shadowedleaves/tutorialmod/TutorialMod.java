942c2d0f8ff12b4902555e2b8b08716fc2ef0b16
ShadowedLeaves <chenmingachang@gmail.com>
2024-09-07 18:08:13 -0700

    Modified Loot Tables

--- src/main/java/net/shadowedleaves/tutorialmod/TutorialMod.java
+++ src/main/java/net/shadowedleaves/tutorialmod/TutorialMod.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.shadowedleaves.tutorialmod.block.ModBlocks;
 import net.shadowedleaves.tutorialmod.item.ModItemGroups;
 import net.shadowedleaves.tutorialmod.item.ModItems;
+import net.shadowedleaves.tutorialmod.util.ModLootTableModifiers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +19,7 @@ public class TutorialMod implements ModInitializer {
 		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+		ModLootTableModifiers.modifyLootTables();
 		FuelRegistry.INSTANCE.add(ModItems.COAL_BRIQUETTE, 200);
 	}
 }
