eed9483ffe2bd9f07d18aedf1cb56afda3ae2587
ShadowedLeaves <chenmingachang@gmail.com>
2024-09-12 00:58:15 -0700

    Added REI Compatability

--- src/main/java/net/shadowedleaves/tutorialmod/compat/TutorialModREIClientPlugin.java
+++ src/main/java/net/shadowedleaves/tutorialmod/compat/TutorialModREIClientPlugin.java
@@ -1,24 +1,34 @@
 package net.shadowedleaves.tutorialmod.compat;
 
+import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import net.shadowedleaves.tutorialmod.block.ModBlocks;
+import net.shadowedleaves.tutorialmod.recipe.GemPolishingRecipe;
+import net.shadowedleaves.tutorialmod.screen.GemPolishingScreen;
+
 
 public class TutorialModREIClientPlugin implements REIClientPlugin {
 
     @Override
     public void registerCategories(CategoryRegistry registry) {
-        REIClientPlugin.super.registerCategories(registry);
+        registry.add(new GemPolishingCategory());
+
+        registry.addWorkstations(GemPolishingCategory.GEM_POLISHING, EntryStacks.of(ModBlocks.GEM_POLISHING_STATION));
     }
 
     @Override
     public void registerDisplays(DisplayRegistry registry) {
-        REIClientPlugin.super.registerDisplays(registry);
+        registry.registerRecipeFiller(GemPolishingRecipe.class, GemPolishingRecipe.Type.INSTANCE,
+                GemPolishingDisplay::new);
     }
 
     @Override
     public void registerScreens(ScreenRegistry registry) {
-        REIClientPlugin.super.registerScreens(registry);
+        registry.registerClickArea(screen -> new Rectangle(75, 30, 20, 30), GemPolishingScreen.class,
+                GemPolishingCategory.GEM_POLISHING);
     }
 }
